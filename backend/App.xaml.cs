8b5085ea7134c0a34a2539d60ea5796595a7f881
HTFI physics <physics.htfi@gmail.com>
2024-01-20 23:28:15 +0900

    タスクトレイへのアイコン追加機能を分離

--- backend/App.xaml.cs
+++ backend/App.xaml.cs
@@ -6,47 +6,31 @@ namespace backend
 
     public partial class App : System.Windows.Application
     {
-        private ContextMenuStrip? _menu;
-        private NotifyIcon? _notifyIcon;
+        private TaskTrayIcon? _taskTrayIcon;
 
         /// <summary>
-        /// 常駐開始時の初期化処理
+        /// 常駐開始時
         /// </summary>
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            _menu = new ContextMenuStrip();
-            _menu.Items.Add("設定", null, (s, e) => {  });
-            _menu.Items.Add("終了", null, (s, e) => { Shutdown(); });
-
-            var icon = GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream;
-            _notifyIcon = new() 
-            {
-                Visible = true,
-                Icon = new Icon(icon),
-                Text = "NotesOnPDF",
-                ContextMenuStrip = _menu,
-            };
-
-            _notifyIcon.MouseClick += (s, e) =>
-            {
-                if (e.Button == MouseButtons.Left)
-                {
+            _taskTrayIcon = new(
+                onClick: () => {
                     // TODO ウィンドウが複数表示されないようにする
                     // TODO アプリを複数起動できないようにする: 
-                }
-            };
+                },
+                onExit: Shutdown
+                );
 
         }
 
         /// <summary>
-        /// 常駐終了時の処理
+        /// 常駐終了時
         /// </summary>
         protected override void OnExit(ExitEventArgs e)
         {
-            _menu?.Dispose();
-            _notifyIcon?.Dispose();
+            _taskTrayIcon?.Dispose();
             base.OnExit(e);
         }
     }
---
+++ backend/TaskTrayIcon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend
+{
+    class TaskTrayIcon : IDisposable
+    {
+        private readonly ContextMenuStrip _menu;
+        private readonly NotifyIcon _notifyIcon;
+
+        public TaskTrayIcon(Action onClick, Action onExit)
+        {
+            _menu = new ContextMenuStrip();
+            _menu.Items.Add("終了", null, (s, e) => { onExit(); });
+
+            var icon = System.Windows.Application.GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream;
+            _notifyIcon = new()
+            {
+                Visible = true,
+                Icon = new Icon(icon),
+                Text = "NotesOnPDF",
+                ContextMenuStrip = _menu,
+            };
+            _notifyIcon.MouseClick += (s, e) =>
+            {
+                if (e.Button != MouseButtons.Left) return;
+                onClick();
+            };
+        }
+
+        public void Dispose()
+        {
+            _menu?.Dispose();
+            _notifyIcon?.Dispose();
+        }
+    }
+}
