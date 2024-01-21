941ee248e002b2e94160329abb8ffa5950cd1bc3
HTFI physics <physics.htfi@gmail.com>
2024-01-21 10:11:19 +0900

    TaxkTrayIconのリファクタリング

--- backend/TaskTrayIcon.cs
+++ backend/TaskTrayIcon.cs
@@ -13,28 +13,45 @@ namespace backend
 
         public TaskTrayIcon(Action onClick, Action onExit)
         {
-            _menu = new ContextMenuStrip();
-            _menu.Items.Add("終了", null, (s, e) => { onExit(); });
-
-            var icon = System.Windows.Application.GetResourceStream(new Uri("icon.ico", UriKind.Relative)).Stream;
-            _notifyIcon = new()
-            {
-                Visible = true,
-                Icon = new Icon(icon),
-                Text = "NotesOnPDF",
-                ContextMenuStrip = _menu,
-            };
-            _notifyIcon.MouseClick += (s, e) =>
-            {
-                if (e.Button != MouseButtons.Left) return;
-                onClick();
-            };
+            _menu = GetMenu(onExit);
+            _notifyIcon = GetNotifyIcon("icon.ico", _menu, onClick);
         }
 
         public void Dispose()
         {
             _menu?.Dispose();
             _notifyIcon?.Dispose();
+        }
+
+
+        //|
+        //| static private
+        //|
+
+        static ContextMenuStrip GetMenu(Action onExit)
+        {
+            var menu = new ContextMenuStrip();
+            menu.Items.Add("終了", null, (s, e) => { onExit(); });
+            return menu;
+        }
+
+        static NotifyIcon GetNotifyIcon(string iconFile, ContextMenuStrip menu, Action onClick)
+        {
+            var icon = System.Windows.Application.GetResourceStream(new Uri(iconFile, UriKind.Relative)).Stream;
+            var notifyIcon = new NotifyIcon()
+            {
+                Visible = true,
+                Icon = new Icon(icon),
+                Text = "NotesOnPDF",
+                ContextMenuStrip = menu,
+            };
+            notifyIcon.MouseClick += (s, e) =>
+            {
+                if (e.Button != MouseButtons.Left) return;
+                onClick();
+            };
+
+            return notifyIcon;
         }
     }
 }
