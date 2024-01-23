afdcd905b0adacd8b9fa5f0d1fd79f7c86aff091
HTFI physics <physics.htfi@gmail.com>
2024-01-23 23:02:03 +0900

    設定ファイルを追加

--- backend/MainWindowVM.cs
+++ backend/MainWindowVM.cs
@@ -9,15 +9,45 @@ using System.Threading.Tasks;
 
 namespace backend
 {
+    // TODO ポートが変わったら、サーバーに再起動が必要
+    // フォルダが存在するかのバリデーションが必要
+
+
     internal partial class MainWindowVM : ObservableObject
     {
         [ObservableProperty]
-        private string name = "abcdefg";
+        private string? rootDirectory;
+
+        [ObservableProperty]
+        private string? notesDirectory;
+
+        [ObservableProperty]
+        private int? portIndex;
 
         [RelayCommand]
-        private void Greet(string user)
+        private void UpdateSettings()
         {
-            Debug.WriteLine($"Hello {user}!");
+            if (RootDirectory == null || NotesDirectory == null || PortIndex == null) return;
+            Properties.Settings.Default.RootDirectory = RootDirectory;
+            Properties.Settings.Default.NotesDirectory = NotesDirectory;
+            Properties.Settings.Default.Port = ports[(int)PortIndex];
+            Properties.Settings.Default.Save();
         }
+
+
+        [RelayCommand]
+        private void ResetSettings()
+        {
+            RootDirectory = Properties.Settings.Default.RootDirectory;
+            NotesDirectory = Properties.Settings.Default.NotesDirectory;
+            PortIndex = ports.ToList().IndexOf( Properties.Settings.Default.Port);
+        }
+
+        public MainWindowVM()
+        {
+            ResetSettings();
+        }
+
+        static readonly string[] ports = ["8080", "8081", "8082", "8083", "8084", "8085"];
     }
 }
