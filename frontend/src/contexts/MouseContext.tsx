f0a4181618c9a237a0470353f8fed62a25745ad0
HTFI physics <physics.htfi@gmail.com>
2024-05-02 23:38:55 +0900

    修正：余白がゼロの時に注釈の追加位置がおかしくなる

--- frontend/src/contexts/MouseContext.tsx
+++ frontend/src/contexts/MouseContext.tsx
@@ -82,20 +82,20 @@ function getRect(
     viewH: number,
     viewX: number
   ) {
-    const H = viewH / (1 - offsetTop - offsetBottom);
-    const W = pdfRatio * H;
-    const ratio = Math.min(1, viewW / W); // 画像が横にはみ出しそうな場合にこの係数をかけて収める
-    const top = H * offsetTop;
-    const bottom = H * offsetBottom;
-    const tbRatio =
-      ratio * H < viewH
-        ? 0 // ページ内に収まる場合は中央に配置する
-        : 1 - ((1 - ratio) * H) / (top + bottom); // ratio==1の時に 1, ratio*H==viewH(==H-top-bottom) の時（ページサイズ＝画像サイズ）に 0 になる
-    const x = viewX + (viewW - ratio * W) / 2;
+    const imgH = viewH / (1 - offsetTop - offsetBottom);
+    const imgW = pdfRatio * imgH;
+    const top = imgH * offsetTop;
+    const bottom = imgH * offsetBottom;
+    const ratio = Math.min(1, viewW / imgW); // 画像が横にはみ出しそうな場合にこの係数をかけて縮小する
+    const ratioTB = // top, bottomにかける係数
+      ratio * imgH <= viewH
+        ? 0 // 画像の縦方向もページ内に収まる場合は中央に配置する
+        : (ratio * imgH - viewH) / (top + bottom); // 縦方向がはみ出す場合（分母が0の時はここには来ない）。ratio==1の時に 1, ratio*imgH==viewH の時に 0
+    const x = viewX + (viewW - ratio * imgW) / 2;
     return {
-      pageRect: new DOMRect(x, -tbRatio * top, ratio * W, ratio * H),
-      top: -tbRatio * top,
-      bottom: -tbRatio * bottom,
+      pageRect: new DOMRect(x, -ratioTB * top, ratio * imgW, ratio * imgH),
+      top: -ratioTB * top,
+      bottom: -ratioTB * bottom,
     };
   }
 }
