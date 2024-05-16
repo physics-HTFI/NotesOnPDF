4aa31235b7668e2392a36939a79814059760f351
HTFI physics <physics.htfi@gmail.com>
2024-05-16 22:19:54 +0900

    ツリー外ファイルの選択ダイアログ表示時のデザインを改良

--- frontend/src/components/OpenFileDrawer/Header/Header.tsx
+++ frontend/src/components/OpenFileDrawer/Header/Header.tsx
@@ -1,5 +1,12 @@
 import { useContext, useState } from "react";
-import { Box, IconButton, Tooltip } from "@mui/material";
+import {
+  Alert,
+  Backdrop,
+  Box,
+  IconButton,
+  Snackbar,
+  Tooltip,
+} from "@mui/material";
 import {
   FolderOpen,
   Language,
@@ -30,6 +37,7 @@ export default function Header({
   const [openUrl, setOpenUrl] = useState(false);
   const [openHistory, setOpenHistory] = useState(false);
   const [downloading, setDownloading] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
 
   const handleSelectExternalPdf = IS_MOCK
     ? () => {
@@ -52,6 +60,7 @@ export default function Header({
           .catch(() => undefined);
       }
     : () => {
+        setOpenAlert(true);
         model
           .getIdFromExternalFile()
           .then((id) => {
@@ -60,6 +69,9 @@ export default function Header({
           })
           .catch(() => {
             setAccessFailedReason("PDFファイルの取得");
+          })
+          .finally(() => {
+            setOpenAlert(false);
           });
       };
 
@@ -140,6 +152,23 @@ export default function Header({
           </IconButton>
         </span>
       </Tooltip>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={openAlert}
+      >
+        <Snackbar open>
+          <Alert
+            elevation={6}
+            variant="filled"
+            severity="info"
+            sx={{ width: "100%" }}
+          >
+            ファイルを選択してください
+            <br />
+            選択ダイアログは、ブラウザの後ろに隠れていることがあります
+          </Alert>
+        </Snackbar>
+      </Backdrop>
 
       {/* URLから開く */}
       <Tooltip title="URLからPDFファイルを開きます">
