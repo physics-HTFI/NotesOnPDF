b3ef059f96f0c029a0a7d88f1bbe1f658d4aafff
HTFI physics <physics.htfi@gmail.com>
2024-05-25 17:34:21 +0900

    Model.Desktopのリファクタリング

--- frontend/src/models/Model.Desktop.tsx
+++ frontend/src/models/Model.Desktop.tsx
@@ -6,11 +6,11 @@ import AppSettings, { GetAppSettings_default } from "@/types/AppSettings";
 import History from "@/types/History";
 import { PageSize } from "@/contexts/PdfNotesContext";
 
-const ORIGIN = import.meta.env.DEV
-  ? "http://localhost:8080"
-  : window.location.href.match(/.*:\d+/)?.[0];
-
 export default class ModelDesktop implements IModel {
+  private origin = import.meta.env.DEV
+    ? "http://localhost:8080"
+    : window.location.href.match(/.*:\d+/)?.[0];
+
   getFlags = () => ({
     canToggleReadOnly: true,
     canOpenHistory: true,
@@ -28,28 +28,28 @@ export default class ModelDesktop implements IModel {
   );
 
   getFileTree = async (): Promise<FileTree> => {
-    const res = await fetch(ORIGIN + "/api/file-tree");
+    const res = await fetch(this.origin + "/api/file-tree");
     if (!res.ok) return Promise.reject();
     const fileTree = (await res.json()) as FileTree;
     return fileTree;
   };
 
   getHistory = async (): Promise<History> => {
-    const res = await fetch(ORIGIN + "/api/history");
+    const res = await fetch(this.origin + "/api/history");
     if (!res.ok) return Promise.reject();
     const history = (await res.json()) as History;
     return history;
   };
   updateHistory = () => Promise.reject();
   clearHistory = async () => {
-    const res = await fetch(ORIGIN + "/api/history", {
+    const res = await fetch(this.origin + "/api/history", {
       method: "DELETE",
     });
     if (!res.ok) return Promise.reject();
   };
 
   getIdFromExternalFile = async (): Promise<string> => {
-    const res = await fetch(ORIGIN + "/api/external-pdf-id");
+    const res = await fetch(this.origin + "/api/external-pdf-id");
     if (!res.ok) return Promise.reject();
     const id = (await res.json()) as string;
     return id;
@@ -57,7 +57,7 @@ export default class ModelDesktop implements IModel {
 
   getIdFromUrl = async (url: string): Promise<string> => {
     const res = await fetch(
-      ORIGIN + `/api/web-pdf-id?${encodeURIComponent(url)}`
+      this.origin + `/api/web-pdf-id?${encodeURIComponent(url)}`
     );
     if (!res.ok) return Promise.reject();
     const id = (await res.json()) as string;
@@ -67,12 +67,12 @@ export default class ModelDesktop implements IModel {
   getFileFromId = () => Promise.reject();
 
   getCoverages = async (): Promise<Coverages> => {
-    const res = await fetch(ORIGIN + "/api/coverage");
+    const res = await fetch(this.origin + "/api/coverage");
     if (!res.ok) return Promise.reject();
     return ((await res.json()) ?? GetCoverages_empty()) as Coverages;
   };
   putCoverages = async (coverages: Coverages): Promise<void> => {
-    const res = await fetch(ORIGIN + "/api/coverage", {
+    const res = await fetch(this.origin + "/api/coverage", {
       method: "PUT",
       body: JSON.stringify(coverages, null, 2),
     });
@@ -80,7 +80,9 @@ export default class ModelDesktop implements IModel {
   };
 
   getPdfNotes = async (id: string): Promise<ResultGetPdfNotes> => {
-    const res = await fetch(ORIGIN + `/api/notes/${encodeURIComponent(id)}`);
+    const res = await fetch(
+      this.origin + `/api/notes/${encodeURIComponent(id)}`
+    );
     if (!res.ok) return Promise.reject();
     const { name, pageSizes, pdfNotes } = (await res.json()) as {
       name: string;
@@ -94,7 +96,7 @@ export default class ModelDesktop implements IModel {
     };
   };
   putPdfNotes = async (id: string, pdfNotes: PdfNotes): Promise<void> => {
-    const res = await fetch(ORIGIN + `/api/notes/${id}`, {
+    const res = await fetch(this.origin + `/api/notes/${id}`, {
       method: "PUT",
       body: JSON.stringify(pdfNotes),
     });
@@ -102,17 +104,17 @@ export default class ModelDesktop implements IModel {
   };
 
   getPageImageUrl = (id: string, page: number, width: number, height: number) =>
-    ORIGIN + `/api/images/${id}/${page}?width=${width}&height=${height}`;
+    this.origin + `/api/images/${id}/${page}?width=${width}&height=${height}`;
 
   getAppSettings = async (): Promise<AppSettings> => {
-    const res = await fetch(ORIGIN + "/api/settings");
+    const res = await fetch(this.origin + "/api/settings");
     if (!res.ok) return Promise.reject();
     const appSettings = ((await res.json()) ??
       GetAppSettings_default()) as AppSettings;
     return appSettings;
   };
   putAppSettings = async (appSettings: AppSettings): Promise<void> => {
-    const res = await fetch(ORIGIN + "/api/settings", {
+    const res = await fetch(this.origin + "/api/settings", {
       method: "PUT",
       body: JSON.stringify(appSettings, null, 2),
     });
