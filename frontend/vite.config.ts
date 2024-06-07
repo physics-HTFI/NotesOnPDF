4929e8293ce57649b308544efa76e701c1f1ac22
HTFI physics <physics.htfi@gmail.com>
2024-06-07 12:49:56 +0900

    baseパスを修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/",
+  base: mode === "web" ? "/NotesOnPDF/" : "/",
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
-});
+}));
