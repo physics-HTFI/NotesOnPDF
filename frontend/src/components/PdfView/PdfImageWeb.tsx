3ca98593e49d198bdc8036bb586af3fa8dd94822
HTFI physics <physics.htfi@gmail.com>
2024-06-08 16:48:05 +0900

    react-pdfのバージョンをアップデート

--- frontend/src/components/PdfView/PdfImageWeb.tsx
+++ frontend/src/components/PdfView/PdfImageWeb.tsx
@@ -10,7 +10,7 @@ import PdfNotesContext, {
 } from "@/contexts/PdfNotesContext/PdfNotesContext";
 
 if (import.meta.env.MODE === "web") {
-  pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+  pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 }
 const options = {
   cMapUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/cmaps/`,
